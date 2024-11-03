94c759f2e158d1387ce6fdf24d6f79c7ef657e57
Ronmycode <67519456+Ronmycode@users.noreply.github.com>
2024-11-03 03:05:34 -0600

    Added ReservationDetails to the project, where user should be able to confirm or cancel reservation trough the API, i wasn't able to confirm the patch endpoint was working tho

--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -1,51 +1,61 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Sidebar from '../components/Sidebar/Sidebar';
-import Login from '../pages/Login/Login'
-import Accommodations from '../pages/Accommodation/Accommodations'
-import Reservations from '../pages/Reservations/Reservations'
-import AccommodationDetails from '../pages/Accommodation/AccommodationDetails'
-import AccommodationsForm from '../pages/Accommodation/AccommodationsForm'
-import ReservationDetails from '../pages/Reservations/ReservationDetails'
-import ReservationForm from '../pages/Reservations/ReservationForm'
-import ReservationNew from '../pages/Reservations/ReservationNew';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sidebar from "../components/Sidebar/Sidebar";
+import Login from "../pages/Login/Login";
+import Accommodations from "../pages/Accommodation/Accommodations";
+import Reservations from "../pages/Reservations/Reservations";
+import AccommodationDetails from "../pages/Accommodation/AccommodationDetails";
+import AccommodationsForm from "../pages/Accommodation/AccommodationsForm";
+import ReservationForm from "../pages/Reservations/ReservationForm";
+import ReservationNew from "../pages/Reservations/ReservationNew";
+import ReservationDetails from "../pages/Reservations/ReservationDetails";
 
 export default function Rutas() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          {/* Ruta de Login sin el Sidebar */}
-          <Route path="/" element={<Login />} />
-          
-          {/* Rutas protegidas que muestran el Sidebar */}
-          <Route
-            path="/*"
-            element={
-              <div className='d-flex flex-row'>
-                <div className='col-2'>
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Ruta de Login sin el Sidebar */}
+        <Route path="/" element={<Login />} />
+
+        {/* Rutas protegidas que muestran el Sidebar */}
+        <Route
+          path="/*"
+          element={
+            <div className="d-flex flex-row">
+              <div className="col-2">
                 <Sidebar />
-
-                </div>
-                <div className='col-10'>
-                  <Routes>
-                    {/* Start Accommodations routes */}
-                    <Route path="Accommodations" element={<Accommodations />} />
-                    <Route path="Accommodation/:id" element={<AccommodationDetails />} />
-                    <Route path="Accommodation-update/:id" element={<AccommodationsForm />} />
-                    {/* End Accommodations routes */}
-  
-                    {/* Start Reservations routes */}
-                    <Route path="reservations" element={<Reservations />} />
-                    <Route path="reservation/:id" element={<ReservationDetails />} />
-                    <Route path="reservation-update/:id" element={<ReservationForm />} />
-                    <Route path="reservation-new" element={<ReservationNew />} />
-
-                  </Routes>
-                </div>
               </div>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+              <div className="col-10">
+                <Routes>
+                  {/* Start Accommodations routes */}
+                  <Route path="Accommodations" element={<Accommodations />} />
+                  <Route
+                    path="Accommodation/:id"
+                    element={<AccommodationDetails />}
+                  />
+                  <Route
+                    path="Accommodation-update/:id"
+                    element={<AccommodationsForm />}
+                  />
+                  {/* End Accommodations routes */}
+
+                  {/* Start Reservations routes */}
+                  <Route path="reservations" element={<Reservations />} />
+                  <Route
+                    path="ReservationDetails/:id"
+                    element={<ReservationDetails />}
+                  />
+                  <Route
+                    path="reservation-update/:id"
+                    element={<ReservationForm />}
+                  />
+                  <Route path="reservation-new" element={<ReservationNew />} />
+                </Routes>
+              </div>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
